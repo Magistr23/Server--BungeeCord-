68c98a854de94d44700dc8f03fc30dbef703d8bc
Иван Сорокин <spanhbob98@mail.ru>
2023-05-19 18:46:10 +0300

    init

---
+++ BungeeCord/start.bat
@@ -0,0 +1,1 @@
+java -Xmx1024M --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.lang.invoke=ALL-UNNAMED -jar BungeeCord.jar nogui
